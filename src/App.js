42f07edfa29187e62c0b54610525d0b17acff35c
Shyla <shailajasrinivas91@gmail.com>
2023-01-17 19:36:20 -0500

    Component  "PostsList' placed in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import PostsList from "./features/posts/PostsList";
 
 function App() {
-	return <main className="App"></main>;
+	return (
+		<main className="App">
+			<PostsList />
+		</main>
+	);
 }
 
 export default App;
