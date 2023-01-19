5d7ac4ac70f52d16a3e67f1bdd94c34ca43b5623
Shyla <shailajasrinivas91@gmail.com>
2023-01-19 16:11:58 -0500

    test case updated to check if the field can be filled out

--- src/App.test.js
+++ src/App.test.js
@@ -19,6 +19,29 @@ test("inputs should be initially empty", () => {
 	expect(titleElement.value).toBe("");
 
 	const select = screen.getByLabelText(/Author:/i);
+	expect(select.value).toBe("");
+
+	const contentElement = screen.getByLabelText(/Content:/i);
+	expect(contentElement.value).toBe("");
+});
+
+//userEvent test - type title
+test("should be able to type a title", () => {
+	const titleElement = screen.getByLabelText(/Post Title:/i);
+	userEvent.type(titleElement, "title");
+	expect(titleElement.value).toBe("title");
+});
+
+//userEvent test - type content
+test("should be able to type a content", () => {
+	const contentElement = screen.getByLabelText(/Content:/i);
+	userEvent.type(contentElement, "content");
+	expect(contentElement.value).toBe("content");
+});
+
+//userEvent test - type content
+test("should be able to select a Author from dropdown", () => {
+	const select = screen.getByLabelText(/Author:/i);
 	const option = screen.getByRole("option", { name: "Aarna S" });
 	userEvent.selectOptions(select, option);
 	expect(select.value).toBe("2");
